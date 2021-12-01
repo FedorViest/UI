27185d6df74df97b93bf415727deea81a46baa86
Fedor Viest <56040832+FedorViest@users.noreply.github.com>
2021-12-01 23:13:36 +0100

    Finished 3rd submission

--- zad_3/main.py
+++ zad_3/main.py
@@ -5,6 +5,16 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 
 
+def final_print(solution, path):
+    print("\n\n")
+    print_map(solution)
+    number = 1
+    print()
+    for i in path:
+        print("\tRoute number: " + str(number) + "\tRoute length: " + str(len(i)) + "\n\t\t==>\t" + str(i) + "\n\n")
+        number += 1
+
+
 def start(file_name, tabu_size, run, threshold):
     garden = create_map("subor" + str(file_name))
     rocks = print_map(garden)
@@ -16,13 +26,6 @@ def start(file_name, tabu_size, run, threshold):
     print("\nDimensions:\t" + str(width) + "x" + str(height) + "\tRocks: " + str(rocks))
 
     solution, path, fitness, best_fitness, generations = tabu_search(garden, tabu_size, max_tiles, height, width, threshold)
-    print("\n\n")
-    print_map(solution)
-    number = 1
-    print()
-    for i in path:
-        print("\tRoute number: " + str(number) + "\tRoute length: " + str(len(i)) + "\n\t\t==>\t" + str(i) + "\n\n")
-        number += 1
 
     plt.plot(generations, fitness, label='Fitness', color='red')
     plt.legend()
@@ -34,7 +37,17 @@ def start(file_name, tabu_size, run, threshold):
               " Generations: " + str(generations[len(generations) - 1]) + " Threshold: " + str(threshold))
     plt.show()
 
+    print()
+    handle_print = str(input("Do you want to print final map and paths? [y/n] "))
+    if handle_print == "y":
+        final_print(solution, path)
+
     return generations[len(generations) - 1]
+
+
+"""
+Algoritmus na vykonanie tabu search
+"""
 
 
 def tabu_search(garden, tabu_size, tiles, height, width, threshold, gens=3000):
@@ -47,9 +60,10 @@ def tabu_search(garden, tabu_size, tiles, height, width, threshold, gens=3000):
     path, best_path = [], []
     all_fitness, all_best_fitness, generations = [], [], []
     while True:
-        if gen == gens:
+        if gen == gens:         # Podmienka na breaknutie cyklu v pripade, ze nastane predvoleny pocet generacii
             return best_garden, best_path, all_fitness, all_best_fitness, generations
         iterator = 0
+        # Cyklus na generovanie susedov
         while True:
             if iterator == threshold:
                 break
@@ -59,10 +73,13 @@ def tabu_search(garden, tabu_size, tiles, height, width, threshold, gens=3000):
             fitnesses.append([get_fitness(temp, tiles), neighbour, temp, temp_path])
             iterator += 1
 
-        sorted_borders = sorted(fitnesses, key=lambda x: x[0])
+        sorted_borders = sorted(fitnesses, key=lambda x: x[0])  # Zoradenie map podla fitness hodnoty od najmensej po najvacsiu
+        fitnesses = []
         filled_map = []
+        # Kontrola ci je tabu list plny
         if len(tabu_list) >= tabu_size:
             tabu_list.pop(0)
+        # Kontrolovanie, ci sa mapa nachadza v tabu liste, ak nie, prida sa do tabu listu
         for curr in sorted_borders:
             if map_to_string(curr[1]) in tabu_list:
                 continue
@@ -77,10 +94,12 @@ def tabu_search(garden, tabu_size, tiles, height, width, threshold, gens=3000):
         all_fitness.append(fitness)
         all_best_fitness.append(best_garden_fitness)
 
+        # Porovnanie, ci je novo vygenerovana zahrada lepsia ako doteraz najlepsia
         if fitness < best_garden_fitness:
             best_garden = deepcopy(filled_map)
             best_garden_fitness = fitness
             best_path = path
+        # V pripade, ze su vsetky policka pohrabane, program sa skonci
         if best_garden_fitness == 0:
             generations.append(gen)
             return best_garden, best_path, all_fitness, all_best_fitness, generations
@@ -96,6 +115,11 @@ def map_to_string(garden):
     for i in garden:
         string = string + str(i)
     return string
+
+
+"""
+Funkcia na vygenerovanie 2 nahodnych indexov z pola a vymenenim pozicii tychto indexov
+"""
 
 
 def get_neighbour(border):
@@ -114,6 +138,11 @@ def get_fitness(garden, tiles):
             if x != "0":
                 fitness -= 1
     return fitness
+
+
+"""
+Funkcia na vyriesenie zahrady zavolanim funkcie move()
+"""
 
 
 def solve(garden, border, height, width):
@@ -175,6 +204,11 @@ def print_map(matrix):
     return rocks
 
 
+"""
+Funkcie na kontrolovanie moznych pohybov do stran
+"""
+
+
 def check_down(garden, height, x, y):
     if y + 1 < height and garden[y + 1][x] == "0":
         return 1
@@ -199,6 +233,11 @@ def check_left(garden, x, y):
     return 0
 
 
+"""
+Funkcia na zistenie moznych pohybov a vybranie jedneho nahodneho z nich
+"""
+
+
 def possible_moves(garden, start, height, width):
     directions = []
     x = start[1]
@@ -216,6 +255,11 @@ def possible_moves(garden, start, height, width):
     return direction
 
 
+"""
+Funkcia na kontrolu, ci som na hranici
+"""
+
+
 def check_border(x, y, height, width):
     if y == height - 1:
         return 1
@@ -226,6 +270,11 @@ def check_border(x, y, height, width):
     if x == 0:
         return 1
     return 0
+
+
+"""
+Funkcia na vykonanie pohybu
+"""
 
 
 def make_move(direction, x, y):
@@ -240,10 +289,20 @@ def make_move(direction, x, y):
     return y, x
 
 
+"""
+Funkcia na skontrolovanie, ci je pozicia mimo vyhradenej mapy
+"""
+
+
 def check_map_borders(x, y, height, width):
     if (x >= width or x < 0) or y >= height or y < 0:
         return 1
     return 0
+
+
+"""
+Funkcia na zmenenie pohybu v pripade, ze cesta nemoze pokracovat, kvoli bud kamenu alebo uz pohrabanemu policku
+"""
 
 
 def move_left_right(garden, direction, x, y, height, width):
@@ -260,6 +319,9 @@ def move_left_right(garden, direction, x, y, height, width):
     if check_map_borders(new_x, new_y, height, width) or garden[new_y][new_x] != "0":
         right = 0
 
+    # Kontrola, ci je mozny pohyb dolava, doprava alebo aj aj. V pripade, ze su mozne oba pohyby, nahodne sa vyberie
+    # pohyb
+    # V pripade, ze sa nemoze pohnut nikam, vrati sa -1
     selections = []
     if left and right:
         selections.extend((r_direction, l_direction))
@@ -272,31 +334,36 @@ def move_left_right(garden, direction, x, y, height, width):
     return -1
 
 
+"""
+Funkcia na vykonanie pohybov a potupne zaplnovanie zahrady pohybmi
+"""
+
+
 def move(garden, start, height, width, move_count):
     iterations = 0
     path = [start]
     x = path[0][1]
     y = path[0][0]
-    direction = possible_moves(garden, start, height, width)
+    direction = possible_moves(garden, start, height, width)    # Vygenerovanie moznych pohybov
     while 1:
         temp = []
 
-        if check_map_borders(x, y, height, width):
-            if iterations > 2 and len(path) <= 1:
+        if check_map_borders(x, y, height, width):  # Kontrola, ci je pozicia na kraji
+            if iterations > 2 and len(path) <= 1:   # Kontrola, aby musela byt dlzka pohybu aspon 2 dlha
                 return None, None, 1
             path.pop(len(path) - 1)
             return garden, path, 0
 
-        if garden[y][x] != "0":
+        if garden[y][x] != "0":     # V pripade, ze v smere stoji prekazka
             path.pop(len(path) - 1)
             iterations -= 1
             x = path[iterations][1]
             y = path[iterations][0]
-            direction = move_left_right(garden, direction, x, y, height, width)
-            if direction == -1:
-                if len(path) <= 1:
+            direction = move_left_right(garden, direction, x, y, height, width)    # Vygenrovanie novych moznych pohybov
+            if direction == -1:     # V tomto pripade, ziadny dalsi pohyb nebol mozny
+                if len(path) <= 1:  # Kontrola ci je dlzka cesty aspon 2 dlha
                     return None, None, 1
-                if not check_border(x, y, height, width):
+                if not check_border(x, y, height, width):   # Kontrola, ci je koniec cesty na kraji mapy (mnich moze vyjst)
                     path.pop(len(path) - 1)
                     return None, None, 1
                 return garden, path, 0
@@ -314,9 +381,10 @@ def move(garden, start, height, width, move_count):
         y = path[iterations][0]
 
 
-file_name = int(input("Select file number (1-10): "))
+file_name = int(input("Select file number: "))
 tabu_size = int(input("Select tabu list size: "))
 threshold = int(input("Select threshold (amount of generated neighbours): "))
+# TESTOVANIE
 """sizes = [500, 50, 10]
 thresholds = [5, 10, 20]
 for threshold in thresholds:
@@ -327,7 +395,7 @@ for threshold in thresholds:
             avg_gen += start(file_name, tabu_size, i + 1, threshold)
         t_end = time.time()
         avg_gen = avg_gen / 10
-        print("Tabu size: " + str(tabu_size) + " Threshold: " + str(threshold) + " Average generation: " + str(avg_gen))
+        print("\nTabu size: " + str(tabu_size) + "\nNeighbours: " + str(threshold) + "\nAverage generation: " + str(avg_gen))
         print("Average time took:" + str((t_end - t_start) / 10) + "\n\n")"""
 
 start(file_name, tabu_size, 1, threshold)
