f3fb39df7a3dc7da7529492a47493fae1d8e22e2
Fedor Viest <56040832+FedorViest@users.noreply.github.com>
2021-12-02 16:22:49 +0100

    Commented code, added knn_start function for initialising lists and performing tests

--- zad_4/main.py
+++ zad_4/main.py
@@ -7,6 +7,11 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 
 
+"""
+Funkcia na nacitanie bodov zo suboru do listu
+"""
+
+
 def load_coords():
     all_pts = []
     with open("coords.txt", "r") as file:
@@ -29,21 +34,24 @@ def load_coords():
     return all_pts
 
 
-def print_points(sur):
-    for s in sur:
-        print(s)
+"""
+Funkcia na vykreslenie bodov do grafu
+"""
 
 
 def plot_points(points, neighbour_count=0):
     x = []
     y = []
     colors = []
-    for point in points:
+    for point in points:    # Priradenie x a y suradnic a farieb do jednotlivych poli
         x.append(point[0])
         y.append(point[1])
         colors.append(point[2])
 
-    plt.scatter(x, y, c=colors, s=2)
+    if len(points) <= 1000:
+        plt.scatter(x, y, c=colors, s=50)
+    else:
+        plt.scatter(x, y, c=colors, s=2)
     if neighbour_count == 0:
         plt.title("Initial state")
     else:
@@ -51,11 +59,17 @@ def plot_points(points, neighbour_count=0):
     plt.show()
 
 
+"""
+Funkcia na vygenerovanie nahodnych bodov a priradenie do pola, ktore funkcia vracia
+"""
+
+
 def generate_points(amount):
     generated = []
     color = ''
-    percentage = int((amount/4) * 0.99)
+    percentage = int((amount/4) * 0.99)  # Vypocitanie kolko z kazdeho bodu je potrebne vygenerovat
     for i in range(percentage * 4):
+        x, y = 0, 0
         if i % 4 == 0:
             x = random.randrange(-5000, 500)
             y = random.randrange(-5000, 500)
@@ -72,9 +86,9 @@ def generate_points(amount):
             x = random.randrange(-501, 5001)
             y = random.randrange(-501, 5001)
             color = 'purple'
-        generated.append([x, y, color])
+        generated.append([x, y, color])     # Priradenie suradnic a farby do pola
     other = amount - (percentage * 4)
-    for i in range(other):
+    for i in range(other):      # Vygenerovanie zvysneho 1% bodov
         x = random.randrange(-5000, 5001)
         y = random.randrange(-5000, 5001)
         if i % 4 == 0:
@@ -85,27 +99,34 @@ def generate_points(amount):
             color = 'blue'
         elif i % 4 == 3:
             color = 'purple'
-        generated.append([x, y, color])
+        generated.append([x, y, color])     # Priradenie suradnic a farby do pola
 
     return generated
+
+
+"""
+Funkcia na klasifikaciu bodov podla suradnic, kazdemu novemu bodu priradi farbu podla KNN algoritmu
+"""
 
 
 def classify(all_pt, generated_pts, neighbour_count):
     counter = 0
     correct_classify = 0
-    for generated_pt in generated_pts:
+    for generated_pt in generated_pts:      # Prechadzanie vygenerovanych bodov
         distances = []
-        x = generated_pt[0]
-        y = generated_pt[1]
-        for point in all_pt:
-            x1 = point[0]
-            y1 = point[1]
+        x = generated_pt[0]         # Ulozenie x suradnice do premennej
+        y = generated_pt[1]         # Ulozenie y suradnice do premennej
+        for point in all_pt:        # Prechadzanie uz urcenych bodov
+            x1 = point[0]           # Ulozenie x suradnice do premennej
+            y1 = point[1]           # Ulozenie y suradnice do premennej
+            # Vypocitanie vzdialenosti generovaneho bodu od vsetkych urcenych bodov
             distance = math.dist((x, y), (x1, y1))
-            distances.append([distance, point[2]])
-        distances = sorted(distances, key=lambda dist: dist[0])
-        distances = distances[:neighbour_count]
+            distances.append([distance, point[2]])      # Priradenie vzdialenosti spolu s farbou do pola
+        distances = sorted(distances, key=lambda dist: dist[0])     # Zoradenie vzdialenosti od najmensej po najvacsiu
+        distances = distances[:neighbour_count]     # Vybratie prvych K prvkov y utriedeneho pola
         values = []
         red, green, blue, purple = 0, 0, 0, 0
+        # Cyklus na poratanie jednotlivych farieb v poli so vzdialenostami
         for distance in distances:
             if distance[1] == 'red':
                 red += 1
@@ -117,32 +138,43 @@ def classify(all_pt, generated_pts, neighbour_count):
                 purple += 1
         values.extend(([red, "red"], [green, "green"], [blue, "blue"], [purple, "purple"]))
 
+        # Zoradenie poctov farieb od najvacsej po najmensiu
         values = sorted(values, key=lambda count: count[0], reverse=True)
 
+        # Ratanie uspesnosti klasifikatora
         if values[0][1] == generated_pt[2]:
             correct_classify += 1
 
-        all_pt.append([x, y, values[0][1]])
+        all_pt.append([x, y, values[0][1]])     # Priradenie bodu do pola vsetkych uz zaradenych bodov
         counter += 1
         sys.stdout.write(f"\rFinished {counter} points")
     return all_pt, correct_classify
 
 
-initial_points = load_coords()
-plot_points(initial_points)
-amount_gen = 20000
-generated = generate_points(amount_gen)
-neighbours = [31]
-for neighbour in neighbours:
-    test_pts = deepcopy(initial_points)
-    start = time.time()
-    classified_pts, correct = classify(test_pts, generated, neighbour)
-    plot_points(classified_pts, neighbour)
-    end = time.time()
-    percentage = (correct / amount_gen) * 100
-    percentage = float("{:.2f}".format(percentage))
-    print(f"\nClassification with {neighbour} neighbours"
-          f"\nTook {int(end - start)} seconds"
-          f"\nCorrectly classified points according to classifier: {percentage}%")
-    print()
-    print()
+"""
+Funkcia na inicializaciu bodov a vykonanie testov
+"""
+
+
+def knn_start(amount_gen):
+    initial_points = load_coords()
+    plot_points(initial_points)
+    generated = generate_points(amount_gen)
+    neighbours = [1, 3, 7, 15]
+    for neighbour in neighbours:
+        test_pts = deepcopy(initial_points)
+        start = time.time()
+        classified_pts, correct = classify(test_pts, generated, neighbour)
+        plot_points(classified_pts, neighbour)
+        end = time.time()
+        percentage = (correct / amount_gen) * 100
+        percentage = float("{:.2f}".format(percentage))
+        print(f"\nClassification with {neighbour} neighbours"
+              f"\nTook {int(end - start)} seconds"
+              f"\nCorrectly classified points according to classifier: {percentage}%")
+        print()
+        print()
+
+
+point_amount = int(input("Points to generate: "))
+knn_start(point_amount)
